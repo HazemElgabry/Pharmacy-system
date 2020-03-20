7ab9c97c8556845188adf0e52bd732c2946f69a9
shimaa87 <shimaaattia16@yahoo.com>
2020-03-21 01:50:37 +0200

    login_commit

---
+++ Pharmacy System/Pharmacy System/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pharmacy_System.Models
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
