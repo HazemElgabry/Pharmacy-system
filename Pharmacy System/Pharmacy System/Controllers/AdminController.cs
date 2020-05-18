5ff8e4bbd7cb81953f6b21f1076b19d994c61cbd
hazem emad elgabry <61589558+HazemElgabry@users.noreply.github.com>
2020-05-18 06:11:05 +0200

    Admin_Dashboard_V1

--- Pharmacy System/Pharmacy System/Controllers/AdminController.cs
+++ Pharmacy System/Pharmacy System/Controllers/AdminController.cs
@@ -8,8 +8,92 @@ namespace Pharmacy_System.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
+        /* /////////////////////////////////////////
+         * //////////  Admin Dashboard  ///////////
+         * ///////////////////////////////////////        
+         */
         public ActionResult Admin_Dashboard()
+        {
+            return View();
+        }
+
+        /* /////////////////////////////////////////
+         * //////////  Admin Profile  ///////////
+         * ///////////////////////////////////////        
+         */
+        public ActionResult Admin_Profile()
+        {
+            return View();
+        }
+
+        /* /////////////////////////////////////////
+         * //////////  Show Admins  ///////////
+         * ///////////////////////////////////////        
+         */
+        public ActionResult Show_Admins()
+        {
+            return View();
+        }
+
+        /* /////////////////////////////////////////
+         * //////////  Add Admins  ///////////
+         * ///////////////////////////////////////        
+         */
+        public ActionResult Add_Admins()
+        {
+            return View();
+        }
+
+        /* /////////////////////////////////////////
+         * //////////  Show Customers  ///////////
+         * ///////////////////////////////////////        
+         */
+        public ActionResult Show_Customers()
+        {
+            return View();
+        }
+
+        /* /////////////////////////////////////////
+         * //////////  Add Customer  ///////////
+         * ///////////////////////////////////////        
+         */
+        public ActionResult Add_Customers()
+        {
+            return View();
+        }
+
+        /* /////////////////////////////////////////
+         * //////////  Show Medicine  ///////////
+         * ///////////////////////////////////////        
+         */
+        public ActionResult Show_Medicine()
+        {
+            return View();
+        }
+
+        /* /////////////////////////////////////////
+         * //////////  Add Medicine  ///////////
+         * ///////////////////////////////////////        
+         */
+        public ActionResult Add_Medicine()
+        {
+            return View();
+        }
+
+        /* /////////////////////////////////////////
+         * //////////  Show News  ///////////
+         * ///////////////////////////////////////        
+         */
+        public ActionResult Show_News()
+        {
+            return View();
+        }
+
+        /* /////////////////////////////////////////
+         * //////////  Add News  ///////////
+         * ///////////////////////////////////////        
+         */
+        public ActionResult Add_News()
         {
             return View();
         }
