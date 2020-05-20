1de4e701c4298b42cbbfd78e20d8e323dd92ca56
Sandra Girgis <totajo1999@gmail.com>
2020-05-20 23:41:00 +0200

    add

--- Pharmacy System/Pharmacy System/Models/Medicine.cs
+++ Pharmacy System/Pharmacy System/Models/Medicine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,19 @@ namespace Pharmacy_System.Models
     public class Medicine
     {
         public int id { get; set; }
+        [Display(Name = "Medicine Name")]
         public string name { get; set; }
 
         public int Price { get; set; }
+        [Display(Name = "Expire Date")]
         public string expire_date { get; set; }
+        [Display(Name = "Description")]
         public string description { get; set; }
+        [Display(Name = "Import Date")]
         public string import_date { get; set; }
+        [Display(Name = "Black List")]
         public string black_list { get; set; }
+        [Display(Name = "Max Dose")]
         public string max_dose { get; set; }
 
 
