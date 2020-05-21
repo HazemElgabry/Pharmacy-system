4da0301e57088a1f706562942f7a979bfaf61e98
essamadel191 <essam.ade191@gmail.com>
2020-05-21 03:01:57 +0200

    Admin Controller

--- Pharmacy System/Pharmacy System/Controllers/AdminController.cs
+++ Pharmacy System/Pharmacy System/Controllers/AdminController.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Pharmacy_System.Models;
 
 namespace Pharmacy_System.Controllers
 {
     public class AdminController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         /* /////////////////////////////////////////
          * //////////  Admin Dashboard  ///////////
          * ///////////////////////////////////////        
@@ -23,6 +27,7 @@ namespace Pharmacy_System.Controllers
          */
         public ActionResult Admin_Profile()
         {
+
             return View();
         }
 
@@ -32,7 +37,7 @@ namespace Pharmacy_System.Controllers
          */
         public ActionResult Show_Admins()
         {
-            return View();
+            return View(db.Admins.ToList());
         }
 
         /* /////////////////////////////////////////
@@ -41,16 +46,83 @@ namespace Pharmacy_System.Controllers
          */
         public ActionResult Add_Admins()
         {
+
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add_Admins(Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Admins.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Show_Admins");
+            }
+
+            return View(admin);
+        }
+        /* /////////////////////////////////////////
+         * //////////  Edit Admins  ///////////
+         * ///////////////////////////////////////        
+         */
+        [HttpGet]
+        public ActionResult Edit_Admin(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(admin);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit_Admin(Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Show_Admins");
+            }
+
+            return View(admin);
+
+        }
+
+        public ActionResult Delete_Admin(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            db.Admins.Remove(admin);
+            
+            db.SaveChanges();
+            return RedirectToAction("Show_Admins");
+
+        }
         /* /////////////////////////////////////////
          * //////////  Show Customers  ///////////
          * ///////////////////////////////////////        
          */
         public ActionResult Show_Customers()
         {
-            return View();
+            return View(db.Customers.ToList());
         }
 
         /* /////////////////////////////////////////
@@ -60,6 +132,71 @@ namespace Pharmacy_System.Controllers
         public ActionResult Add_Customers()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add_Customers(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Show_Customers");
+            }
+            return View(customer);
+        }
+        /* /////////////////////////////////////////
+         * //////////  Edit Customers  ///////////
+         * ///////////////////////////////////////        
+         */
+        [HttpGet]
+        public ActionResult Edit_customer_as_Admin(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit_customer_as_Admin(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Show_Customers");
+            }
+
+            return View(customer);
+
+        }
+
+        public ActionResult Delete_customer_as_Admin(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Show_Customers");
+
         }
 
         /* /////////////////////////////////////////
